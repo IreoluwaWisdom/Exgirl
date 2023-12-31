ea6501a93a2e2c4d79ba325cf279b89ec7324756
IreoluwaWisdom <ireoluwaolukayode@gmail.com>
2023-12-31 02:42:34 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// App.js
+import React from 'react';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My React App</h1>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+// Home.js
+import React from 'react'; function Home() 
+{
+  return ( <div> <h2>Hello World from 
+      Home</h2>
+    </div> );
+}
+export default Home;
