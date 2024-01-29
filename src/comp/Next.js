38acff109b012e8eee3d331b15ed0f73a3705f02
IreoluwaWisdom <140738967+IreoluwaWisdom@users.noreply.github.com>
2024-01-29 02:10:56 +0100

    Update Next.js

--- src/comp/Next.js
+++ src/comp/Next.js
@@ -1,2 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { MdOutlineNavigateNext } from "react-icons/md";
+
+const Next= (link) => {
+  return (
+    <div style = {{marginBottom:'20vh'}} >
+      <MdOutlineNavigateNext/>
+    </div>
+  );
+};
+export default Next;
