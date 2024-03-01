131d114c73a607fa1275100dd65ae4c4ddf66335
IreoluwaWisdom <140738967+IreoluwaWisdom@users.noreply.github.com>
2024-03-01 16:36:51 +0100

    Update FriedChicken.js

--- src/items/FriedChicken.js
+++ src/items/FriedChicken.js
@@ -5,8 +5,25 @@ import friedchicken from '../assets/fried-chicken.jpg';
 
 const FriedChicken = () => {
   return (
-    <div>
-      <h1>Fried Chicken</h1>
+        <div
+      style={{
+        position: "absolute",
+        textAlign: "center",
+        top: "15%",
+        left: "15%",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>Fried Chicken</h1>
+      <img
+        src={friedchicken}
+        style={{
+          borderRadius: "10px",
+          marginTop: "3vh",
+          marginBottom: "5vh",
+          width: "75vw",
+          height: "50vw",
+        }}
+      />
       <QuantitySelector itemName="Fried Chicken"/>
     </div>
   );
