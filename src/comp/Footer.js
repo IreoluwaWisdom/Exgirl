f599fff92564ec2d5f171bc481eca6a22f3642b5
Ireoluwa Olukayode <wisdomireoluwa@gmail.com>
2024-03-05 10:40:24 +0000

    footer

--- src/comp/Footer.js
+++ src/comp/Footer.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div style = {{paddingBottom:'20vh', paddingTop:'7vh', backgroundColor:'#F0F0F0', textAlign:'center', color :'#6a0dad' }} >
+    <div
+      style={{
+        paddingBottom: "20vh",
+        paddingTop: "7vh",
+        backgroundColor: "#F0F0F0",
+        textAlign: "center",
+        color: "#6a0dad",
+      }}
+    >
       &copy; 2024 Ex-girlfriend foods
-      <br/>
-      Developed by Ireoluwa Olukayode
-
+      <br />
+      <Link to="https://www.upwork.com/freelancers/~011dd99f9d6b3935c9">
+        Developed by Ireoluwa Olukayode
+      </Link>
     </div>
   );
 };
